5f74192dd3502b4c175c0fb5ced5e3744297287b
Reini Urban <rurban@cpan.org>
2018-07-18 13:48:10 +0200

    bump to 2.4#4_2

--- emu/init.c
+++ emu/init.c
@@ -89,7 +89,7 @@ void init_toam(argc, argv)
                     print_picat_usage();
                     exit(0);
                 } else if (*(str+2) == 'v' || strcmp(str+2,"version")==0){
-                    printf("Picat version 2.4#4_1\n");
+                    printf("Picat version 2.4#4_2\n");
                     exit(0);
                 }
 
